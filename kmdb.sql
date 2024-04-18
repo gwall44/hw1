3d84835519cda61586f9c3489d4e68c333b2c5fd
Greta Wall <greta.wall@kellogg.northwestern.edu>
2024-04-18 01:33:26 +0000

    changes2

--- kmdb.sql
+++ kmdb.sql
@@ -111,10 +111,30 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS studios;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released INTEGER,
+  rating TEXT,
+  studio_id INT, FOREIGN KEY (studio_id) 
+);
+
+CREATE TABLE actors (
+  actor_id INT PRIMARY KEY AUTO_INCREMENT,
+  actor_name TEXT,
+);
+
+CREATE TABLE studios (
+    studio_id INT PRIMARY KEY AUTO_INCREMENT,
+    studio_name TEXT,
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
@@ -137,24 +157,4 @@
 
 -- The SQL statement for the cast output
 -- TODO!
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS teachers;
-DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS sections;
-DROP TABLE IF EXISTS enrollments;
 
-CREATE TABLE movies (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT,
-  year_released INTEGER,
-  rating TEXT,
-  studio_name TEXT
-);
-
-
-CREATE TABLE roles (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  first_name TEXT,
-  last_name TEXT,
-  bio TEXT
-);
